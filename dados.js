fcc2916179be709ed3f0e2dacd202df223c4e3ea
Kaio-Costa <kaiodev830@gmail.com>
2024-09-08 00:37:05 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como a Fadinha, é uma skatista brasileira...",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        rede_sociais : "https://www.instagram.com/rayssalealoficial/",
+        esporte : "skatista",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação no esporte. Com suas acrobacias impecáveis e carisma contagiante, ela se destaca no cenário mundial da ginástica.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        rede_sociais : "https://www.instagram.com/rebecarandrade/?hl=pt-br",
+        esporte : "ginasta",
+        tags: "ginástica ginastica ouro"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Tóquio...",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)",
+        rede_sociais : "https://www.instagram.com/bia_souzaa/",
+        esporte : "judoca",
+        tags: "judô judo judoca judoka"
+    }
+];
+
